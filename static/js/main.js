122656448c049cc7125b3ab12753b7e536a7ca13
abelikovstudy <beli@sfedu.ru>
2021-04-14 15:07:20 +0400

    Installed Vue(vue3 + vueCLI), Sass(loaders fow webpack)

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-let doc = document.getElementById('js_tag')
-
-const randInt = (min,max) => {
-    return Math.random() * (max - min) + min
-}
-
-doc.onclick = () => {
-    doc.style.color = `rgb(${randInt(0,255)},${randInt(0,255)},${randInt(0,255)})`
-}
